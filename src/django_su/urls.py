476ffa82ec91a61807edaaa6c0a17d640fa6df69
Alan Justino <alan.justino@yahoo.com.br>
2011-10-31 19:44:41 -0200

    Some log on people su-ing into other account

--- src/django_su/urls.py
+++ src/django_su/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
-from django_su.views import login_as_user, su_exit
+from django_su.views import login_as_user
 
 urlpatterns = patterns("",
     url(r"^(?P<user_id>[\d]+)/$", login_as_user, name="login_as_user"),
--- src/django_su/views.py
+++ src/django_su/views.py
@@ -5,13 +5,18 @@ from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
+import logging
 
 @user_passes_test(lambda u: u.is_staff)
 def login_as_user(request, user_id, can_su_staff=False):
     su_user = get_object_or_404(User, pk=user_id)
+    staff_user = request.user
+    logging.debug('User "%s" trying to log as "%s"' % (staff_user, su_user))
     if (su_user.is_staff or su_user.is_superuser) and not can_su_staff:
+        logging.warn('Forbidden to "%s" to login as "%s"' % (staff_user, su_user))
         return HttpResponseForbidden("Cannot su other staff members")
 
     su_user.backend = settings.AUTHENTICATION_BACKENDS[0]
     login(request, su_user)
+    logging.info('Staff member "%s" successfull logged as "%s"' % (staff_user, su_user))
     return HttpResponseRedirect("/")
