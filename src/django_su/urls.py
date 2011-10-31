c1f51630a6806ec206ec40be301be80876bd8edf
Alan Justino <alan.justino@yahoo.com.br>
2011-10-31 19:11:49 -0200

    Cleaned-up unsafe features

--- src/django_su/urls.py
+++ src/django_su/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls.defaults import *
 from django_su.views import login_as_user, su_exit
 
 urlpatterns = patterns("",
-    url(r"^su/(?P<user_id>[\d]+)/$", login_as_user, name="login_as_user"),
-    url(r"^su/$", su_exit, name="su_exit"),
+    url(r"^(?P<user_id>[\d]+)/$", login_as_user, name="login_as_user"),
 )
