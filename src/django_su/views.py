9652e4c4ec57ea23ffd4e8c429c00afdbdf4856f
Alan Justino <alan.justino@yahoo.com.br>
2011-10-31 19:13:52 -0200

    By default, cannot su staff members

--- src/django_su/views.py
+++ src/django_su/views.py
@@ -7,8 +7,11 @@ from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import login
 
 @user_passes_test(lambda u: u.is_staff)
-def login_as_user(request, user_id):
+def login_as_user(request, user_id, can_su_staff=False):
     su_user = get_object_or_404(User, pk=user_id)
+    if (su_user.is_staff or su_user.is_superuser) and not can_su_staff:
+        return HttpResponseForbidden("Cannot su other staff members")
+
     su_user.backend = settings.AUTHENTICATION_BACKENDS[0]
     login(request, su_user)
     return HttpResponseRedirect("/")
